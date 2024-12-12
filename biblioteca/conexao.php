fed6d78a9d4c2ee597deee0b0b82da09640ce81a
prof-lucassilva <106394691+prof-lucassilva@users.noreply.github.com>
2024-12-12 15:59:12 -0300

    Delete biblioteca directory

--- biblioteca/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbHost = 'db4free.net:3306';
-$dbUsername = 'isaroot';
-$dbPassword = 'isabella0223';
-$dbName = 'localhostisa';
-    
-  $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
-?>
--- biblioteca/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Encerrar a sessão
-session_start();
-session_unset();
-session_destroy();
-header("Location: login.php");
-?>
